9ff0af136cc07498fa996bb2116a935363a74574
Administrator <Administrator@ADMIN.domenfo.com>
2022-07-07 16:32:25 +0300

    Update files

--- models/BudgetModel.php
+++ models/BudgetModel.php
@@ -83,6 +83,30 @@ class BudgetModel extends Model {
                    return $res;
                         
                  break;
+                 
+                        case "four":
+
+                     $sql = "SELECT id, marker_a, marker_b, name, ekr, dmsh, dhsh, u_dmsh, u_dhsh"
+                . " from reporting_budget";
+
+                   $res = [];
+                   $stmt = $this->db->prepare($sql);
+                   $stmt->execute();
+                   
+                   while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                       
+               # Разделяем число на блоки
+               $block = ['dmsh', 'dhsh', 'u_dmsh', 'u_dhsh'];
+               for ($num = 0 ; $num <= 3 ; ++$num) {
+               $row[$block[$num]] = number_format($row[$block[$num]], 2, ',', ' ');
+               }
+                       
+                       $res[$row['id']] = $row;
+                   }
+        
+                   return $res;
+                            
+                   break;
             
            }
 
@@ -364,6 +388,89 @@ class BudgetModel extends Model {
                     }
                     
                    break;
+                   
+                        case "four":
+      
+                                                        $id = $value['id'];
+                              
+                     # Обновляем значения нужных нам учреждений
+                     $sql = "UPDATE reporting_budget SET dmsh = :dmsh, dhsh = :dhsh "
+                         . "WHERE id = '$id'";
+
+                     $stmt = $this->db->prepare($sql);
+                     $stmt->bindValue(":dmsh", $value['dmsh'], PDO::PARAM_STR);
+                     $stmt->bindValue(":dhsh", $value['dhsh'], PDO::PARAM_STR);
+                     
+                     $stmt->execute();
+                     
+                     
+                     # Пересчитываем значение общего пункта
+                     $marker_b = $value['marker_b'];
+                     
+                     $sql = "SELECT SUM(dmsh), SUM(dhsh) from reporting_budget "
+                             . "WHERE marker_b = '$marker_b' AND marker_a = '0'";
+                     
+                   $stmt = $this->db->prepare($sql);
+                   $stmt->execute();
+                   $row = $stmt->fetch(PDO::FETCH_ASSOC);
+                   
+                   $dmsh = $row['SUM(dmsh)'];
+                   $dhsh = $row['SUM(dhsh)'];
+                 
+                   $sql = "UPDATE reporting_budget SET dmsh = '$dmsh', dhsh = '$dhsh'"
+                           . " WHERE marker_a = '10' AND marker_b = '$marker_b'";
+                   
+                    $stmt = $this->db->prepare($sql);
+                    $stmt->execute();
+                    
+
+                    # Пересчитаем строки 240/260/290/340
+                    if($marker_b >= '16' && $marker_b <= '18' || $marker_b >= '20' && $marker_b <= '24' || $marker_b >= '26' && $marker_b <= '31' || $marker_b >= '35' && $marker_b <= '41'){
+                        
+            if($marker_b >= '16' && $marker_b <= '18'){
+                $B = 15;
+                $number1 = 16;
+                $number2 = 18;
+            }
+            
+            if($marker_b >= '20' && $marker_b <= '24'){
+                $B = 19;
+                $number1 = 20;
+                $number2 = 24;
+            }
+            
+            if($marker_b >= '26' && $marker_b <= '31'){
+                $B = 25;
+                $number1 = 26;
+                $number2 = 31;
+            }
+            
+            if($marker_b >= '35' && $marker_b <= '41'){
+                $B = 34;
+                $number1 = 35;
+                $number2 = 41;
+            }
+                       
+                 $sql = "SELECT SUM(dmsh), SUM(dhsh) FROM reporting_budget WHERE "
+                         . "marker_a = '10' AND marker_b BETWEEN '$number1' AND '$number2'";
+                 
+                   $stmt = $this->db->prepare($sql);
+                   $stmt->execute();
+                   $row = $stmt->fetch(PDO::FETCH_ASSOC);
+                   
+                   
+                   $dmsh = $row['SUM(dmsh)'];
+                   $dhsh = $row['SUM(dhsh)'];
+                   
+                   $sql = "UPDATE reporting_budget SET dmsh = '$dmsh', dhsh = '$dhsh' "
+                           . "WHERE marker_a = '10' AND marker_b = '$B'";
+                   
+                           $stmt = $this->db->prepare($sql);
+                           $stmt->execute();
+            
+                    }
+                            
+                        break;
              }
              
          }
@@ -449,6 +556,33 @@ class BudgetModel extends Model {
                    return $res;
                        
                  break;
+                 
+                    case "four":
+      
+                        $sql = "SELECT id, SUM(dmsh), SUM(dhsh), SUM(u_dmsh), SUM(u_dhsh)"
+                . " from reporting_budget WHERE marker_a = '10' AND "
+                    . "id != '118' AND id != '151' AND id != '171' AND id != '197'";
+
+                   $res = [];
+                   $stmt = $this->db->prepare($sql);
+                   $stmt->execute();
+                   
+                   while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                       
+               # Разделяем число на блоки
+               $block = ['SUM(dmsh)', 'SUM(dhsh)', 'SUM(u_dmsh)', 'SUM(u_dhsh)'];
+               for ($num = 0 ; $num <= 3 ; ++$num) {
+               $row[$block[$num]] = number_format($row[$block[$num]], 2, ',', ' ');
+               }
+                       
+                       $res[$row['id']] = $row;
+                   }
+        
+                   return $res;
+                            
+                   break;
+                 
+                 
             
            }
              
--- views/budget_back.php
+++ views/budget_back.php
@@ -347,7 +347,81 @@
        break;
    
        case "four":
-       echo "Четвертое сочетание";
+
+           ?>
+              
+              <table class="freeze-table" width="700px">
+                  
+                  <thead>
+                      <tr>
+                          <th style="min-width: 200px; width: 200px;" class="col-id-no fixed-header">Наименование расходов</th>
+                          <th style="min-width: 70px; width: 70px;">ЭКР</th>
+                          <th style="min-width: 200px; width: 200px;">ДМШ</th>
+                          <th style="min-width: 200px; width: 200px;">ДХШ</th>
+                          <th style="min-width: 200px; width: 200px;">ДМШ</th>
+                          <th style="min-width: 200px; width: 200px;">ДХШ</th>
+                      </tr>
+                  </thead>
+                  
+                  <tbody>
+                      
+                      <?php
+                     
+                      # Этот бред в цикле нужно убрать!!! Переменную J нужно получать иначе!!!
+                      for ($j = 1 ; $j < 44 ; ++$j){
+                          
+                          foreach ($pageData['info'] as $key => $value) {
+
+                              if ($value['marker_a'] == 10 && $value['marker_b'] == $j ) {
+                                  echo "<tr>";
+                                  echo "<td class='col-id-no' scope='row'><b>" . $value['name'] . "</td></b>";
+                                  echo "<td><b>" . $value['ekr'] . "</td></b>";
+                                  echo "<td><b>" . $value['dmsh'] . "</td></b>";
+                                  echo "<td><b>" . $value['dhsh'] . "</td></b>";
+                                  echo "<td><b>" . $value['u_dmsh'] . "</td></b>";
+                                  echo "<td><b>" . $value['u_dhsh'] . "</td></b>";
+                                  echo "</tr>";
+                              }
+                              
+                              if ($value['marker_a'] == 0 && $value['marker_b'] == $j ) {
+                                  echo "<tr>";
+                                  echo "<input type=hidden class='marker_b' value=" . $j . ">";
+                                  echo "<input type=hidden class='id' value=" . $value['id'] . ">";
+                                  echo "<td class='col-id-no' scope='row'>" . $value['name'] . "</td>";
+                                  echo "<td>" . $value['ekr'] . "</td>";
+                                  echo <<<HTML
+                                  <td><input type="text" id='user' class='dmsh' value="$value[dmsh]"></td>
+                                  <td><input type="text" id='user' class='dhsh' value="$value[dhsh]"></td>
+                                  HTML;
+                                  echo "<td>" . $value['u_dmsh'] . "</td>";
+                                  echo "<td>" . $value['u_dhsh'] . "</td>";
+                                  echo "</tr>";
+                              }
+                              
+                          }
+                          
+                      }
+                      
+                      #Итоговая строка
+                      foreach ($pageData['total'] as $key => $value) {
+                      echo "<tr>";
+                      echo "<td class='col-id-no' scope='row'><b>ИТОГО</td>";
+                      echo "<td></td>";
+                      echo "<td><b>" . $value['SUM(dmsh)'] . "</b></td>";
+                      echo "<td><b>" . $value['SUM(dhsh)'] . "</b></td>";
+                      echo "<td><b>" . $value['SUM(u_dmsh)'] . "</b></td>";
+                      echo "<td><b>" . $value['SUM(u_dhsh)'] . "</b></td>";
+                      echo "</tr>";
+                      }
+                      
+                      ?>
+                      
+                  </tbody>
+                  
+              </table>
+              
+              <?php
+           
        break;
    
        case "five":

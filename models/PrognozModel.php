0e903105feb061a6c7cdf2e5a444380bea173de5
Administrator <Administrator@ADMIN.domenfo.com>
2022-07-12 15:27:40 +0300

    Update files

--- models/PrognozModel.php
+++ models/PrognozModel.php
@@ -8,7 +8,13 @@ class PrognozModel extends Model {
  
   public function prognoz_back() {
       
-                      $sql = "SELECT * from reporting_prognoz";
+                      $sql = "SELECT *,(teplo_vol1 + teplo_vol2) AS teplo_vol, (teplo_sum1 + teplo_sum2) AS teplo_sum, "
+                              . "(water_vol1 + water_vol2) AS water_vol, (water_sum1 + water_sum2) AS water_sum, "
+                              . "(stoki_vol1 + stoki_vol2) AS stoki_vol, (stoki_sum1 + stoki_sum2) AS stoki_sum, "
+                              . "(elektro_vol1 + elektro_vol2) AS elektro_vol, (elektro_sum1 + elektro_sum2) AS elektro_sum, "
+                              . "(trash_vol1 + trash_vol2) AS trash_vol, (trash_sum1 + trash_sum2) AS trash_sum, "
+                              . "(negativka_vol1 + negativka_vol2) AS negativka_vol, (negativka_sum1 + negativka_sum2) AS negativka_sum "
+                              . "from reporting_prognoz";
 
                    $res = [];
                    $stmt = $this->db->prepare($sql);
@@ -22,8 +28,10 @@ class PrognozModel extends Model {
                    'stoki_vol1', 'stoki_vol2', 'stoki_sum1', 'stoki_sum2',
                    'elektro_vol1', 'elektro_vol2', 'elektro_sum1', 'elektro_sum2',
                    'trash_vol1', 'trash_vol2', 'trash_sum1', 'trash_sum2',
-                   'negativka_vol1', 'negativka_vol2', 'negativka_sum1', 'negativka_sum2'];
-               for ($num = 0 ; $num <= 23 ; ++$num) {
+                   'negativka_vol1', 'negativka_vol2', 'negativka_sum1', 'negativka_sum2',
+                   'teplo_sum', 'teplo_vol', 'water_vol', 'water_sum', 'stoki_vol', 'stoki_sum',
+                   'elektro_vol', 'elektro_sum', 'trash_vol', 'trash_sum', 'negativka_vol', 'negativka_sum'];
+               for ($num = 0 ; $num <= 35 ; ++$num) {
                $row[$block[$num]] = number_format($row[$block[$num]], 2, ',', ' ');
                }
                        
--- views/prognoz_back.php
+++ views/prognoz_back.php
@@ -59,8 +59,10 @@
                   case "teplo":
                       $volume1 = "teplo_vol1";
                       $volume2 = "teplo_vol2";
+                      $vol = "teplo_vol";
                       $sum1 = "teplo_sum1";
                       $sum2 = "teplo_sum2";
+                      $sum = "teplo_sum";
                       $title = "Теплоснабжение";
                       $tarif1 = $pageData['tarif']['teplo']['tarif1'];
                       $tarif2 = $pageData['tarif']['teplo']['tarif2'];
@@ -69,8 +71,10 @@
                   case "water":
                       $volume1 = "water_vol1";
                       $volume2 = "water_vol2";
+                      $vol = "water_vol";
                       $sum1 = "water_sum1";
                       $sum2 = "water_sum2";
+                      $sum = "water_sum";
                       $title = "Водоснабжение";
                       $tarif1 = $pageData['tarif']['water']['tarif1'];
                       $tarif2 = $pageData['tarif']['water']['tarif2'];
@@ -79,8 +83,10 @@
                   case "stoki":
                       $volume1 = "stoki_vol1";
                       $volume2 = "stoki_vol2";
+                      $vol = "stoki_vol";
                       $sum1 = "stoki_sum1";
                       $sum2 = "stoki_sum2";
+                      $sum = "stoki_sum";
                       $title = "Водоотведение";
                       $tarif1 = $pageData['tarif']['stoki']['tarif1'];
                       $tarif2 = $pageData['tarif']['stoki']['tarif2'];
@@ -89,8 +95,10 @@
                   case "elektro":
                       $volume1 = "elektro_vol1";
                       $volume2 = "elektro_vol2";
+                      $vol = "elektro_vol";
                       $sum1 = "elektro_sum1";
                       $sum2 = "elektro_sum2";
+                      $sum = "elektro_sum";
                       $title = "Электроснабжение";
                       $tarif1 = $pageData['tarif']['elektro']['tarif1'];
                       $tarif2 = $pageData['tarif']['elektro']['tarif2'];
@@ -99,8 +107,10 @@
                   case "trash":
                       $volume1 = "trash_vol1";
                       $volume2 = "trash_vol2";
+                      $vol = "trash_vol";
                       $sum1 = "trash_sum1";
                       $sum2 = "trash_sum2";
+                      $sum = "trash_sum";
                       $title = "Вывоз мусора";
                       $tarif1 = $pageData['tarif']['trash']['tarif1'];
                       $tarif2 = $pageData['tarif']['trash']['tarif2'];
@@ -109,8 +119,10 @@
                   case "negativka":
                       $volume1 = "negativka_vol1";
                       $volume2 = "negativka_vol2";
+                      $vol = "negativka_vol";
                       $sum1 = "negativka_sum1";
                       $sum2 = "negativka_sum2";
+                      $sum = "negativka_sum";
                       $title = "Негативка";
                       $tarif1 = $pageData['tarif']['negativka']['tarif1'];
                       $tarif2 = $pageData['tarif']['negativka']['tarif2'];
@@ -125,10 +137,51 @@
                       <?php
                       echo "<td>" . $title . "</td>";
                       echo <<<HTML
-                      <td><input type="text" id='user' class='aurinko' value="$tarif1"></td>
-                      <td><input type="text" id='user' class='aurinko' value="$tarif2"></td>
+                      <td><input type="text" id='user' class='tarif1' value="$tarif1"></td>
+                      <td><input type="text" id='user' class='tarif2' value="$tarif2"></td>
                       HTML;
                       ?>
                   </tr>
               </table>
+              
+                            <table class="freeze-table" width="700px">
+                  
+                  <thead>
+                      <tr>
+                          <th style="min-width: 200px; width: 200px;" class="col-id-no fixed-header">Учреждение</th>
+                          <th style="min-width: 200px; width: 200px;">Объем 1-полугодие</th>
+                          <th style="min-width: 200px; width: 200px;">Сумма 1-полугодие</th>
+                          <th style="min-width: 200px; width: 200px;">Объем 2-полугодие</th>
+                          <th style="min-width: 200px; width: 200px;">Сумма 2-полугодие</th>
+                          <th style="min-width: 200px; width: 200px;">Объем за год</th>
+                          <th style="min-width: 200px; width: 200px;">Сумма за год</th>
+                      </tr>
+                  </thead>
+                  
+                  <tbody>
+                      
+                      <?php
+                      
+                      foreach ($pageData['info'] as $key => $value) {
+                          
+                          echo "<tr>";
+                          echo "<td>" . $value['full_name'] . "</td>";
+                          echo <<<HTML
+                          <td><input type="text" id='user' class='volume1' value="$value[$volume1]"></td>
+                          HTML;
+                          echo "<td>" . $value[$sum1] . "</td>";
+                          echo <<<HTML
+                          <td><input type="text" id='user' class='volume2' value="$value[$volume2]"></td>
+                          HTML;
+                          echo "<td>" . $value[$sum2] . "</td>";                       
+                          echo "<td>" . $value[$vol] . "</td>";
+                          echo "<td>" . $value[$sum] . "</td>";
+                          echo "</tr>";
+                          
+                      }
+                      
+                      ?>
+                      
+                  </tbody>
+                            </table>
 
